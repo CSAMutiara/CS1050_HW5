48c4b5bf0c742f137236e69bfc7b3e7c1ff5ee87
Ayesha Mutiara <a100017@aup.edu>
2018-03-23 12:26:35 +0000

    LsCommand updated

--- terminal/src/main/java/aup/cs/terminal/LsCommand.java
+++ terminal/src/main/java/aup/cs/terminal/LsCommand.java
@@ -18,20 +18,28 @@ public final class LsCommand extends Command {
     
     public LsCommand(File f) {
         super(f);
-        this.files = deletable.listFiles();
+        this.files = file.listFiles();
     }
     
     /**
      * exec method executes an action.
      */
     public File exec() throws TerminalExecutionException {
-        
         if (files != null) {
             for (File f: files) {
-                f.delete();
+                double bytes = f.length();
+                double date = f.lastModified();
+                if (f.isFile()) {
+                    System.out.println("File: " + f.getName()
+                        + " (last modified: " + date + ") - "
+                        + bytes + " byte");
+                } else if (f.isDirectory()) {
+                    System.out.println("Directory: " + f.getName()
+                        + " (last modified: " + date + ") - "
+                        + bytes + " byte");
+                }
             }
         }
-        deletable.delete();
         return file;
     }
     
